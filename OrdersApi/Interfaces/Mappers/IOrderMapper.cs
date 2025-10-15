808255d8c77fc46cbd64f0204df264281299c835
Mahrez Tah <mtaharboucht@gmail.com>
2025-10-15 15:19:38 +0200

    feat(dtos): implement dto and entites mappers

---
+++ OrdersApi/Interfaces/Mappers/IOrderMapper.cs
@@ -0,0 +1,26 @@
+﻿using OrdersApi.Dtos.Orders;
+using OrdersApi.Entities;
+
+namespace OrdersApi.Interfaces.Mappers
+{
+    /// <summary>
+    /// Order entity/dto mapper. 
+    /// </summary>
+    public interface IOrderMapper
+    {
+        /// <summary>
+        /// Create Order from CreateOrderDto.
+        /// </summary>       
+        Order ToOrderEntity(CreateOrderDto dto);
+
+        /// <summary>
+        /// Create OrderDetailResponseDto from Order.
+        /// </summary>        
+        OrderDetailResponseDto ToOrderDetailResponseDto(Order entity);
+
+        /// <summary>
+        /// Create CreateOrderResponseDto from Order.
+        /// </summary>        
+        CreateOrderResponseDto ToCreateOrderResponseDto(Order entity);
+    }
+}
